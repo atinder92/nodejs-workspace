c6162e93f63a2c034e820e0dec81080335b502b4
Atinder Singh <atinder.s.92@gmail.com>
2017-07-12 11:17:33 +1000

    ExpressJs setup

---
+++ web/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+var port = 8082;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/',function(req,res){
+
+    res.send('Hello World');
+
+
+});
+
+
+app.get('/books',function(req,res){
+
+    res.send('Hello Books');
+
+
+});
+
+app.listen(port,function(err){
+    console.log(`Running server on ${port}`);
+});
