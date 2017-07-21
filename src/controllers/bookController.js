21279048b5f0fa1a122106a4b4650a8a5ed95cd9
Atinder Singh <atinder.s.92@gmail.com>
2017-07-21 15:12:06 +1000

    Using mongoose to find book information

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,7 @@
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectId;
+var mongoose = require('mongoose');
+var BookData = require('../models/books.server.model');
 
 
 var bookController = function(service,nav){
@@ -7,29 +9,43 @@ var bookController = function(service,nav){
     
     var getIndex = function(req,res){
 
-        //connect to database
-        var url = "mongodb://localhost:27017/libraryapp";
-        mongodb.connect(url,function(err,db){
+        // //connect to database
+        // var url = "mongodb://localhost:27017/libraryapp";
+        // mongodb.connect(url,function(err,db){
 
-            var collection = db.collection('books');
+        //     var collection = db.collection('books');
 
-            //get all books
-            collection.find({}).toArray(function(err,results){
-
-
-                  res.render('books',{
-                      books:results,
-                      nav :nav
+        //     //get all books
+        //     collection.find({}).toArray(function(err,results){
 
 
-                   });
+        //           res.render('books',{
+        //               books:results,
+        //               nav :nav
 
 
+        //            });
+
+
+        //     });
+
+
+
+        // });
+
+        //Using Mongoose API to get all documents
+        mongoose.connect('mongodb://localhost:27017/libraryapp');
+
+        BookData.find(function(err,results){
+
+            res.render('books',{
+                books:results,
+                nav:nav
             });
 
 
-
         });
+
 
 
     }
@@ -39,25 +55,41 @@ var bookController = function(service,nav){
         //get id
         var id = new objectId(req.params.id);
 
-        //connect to database
-        var url = "mongodb://localhost:27017/libraryapp";
-        mongodb.connect(url,function(err,db){
+        // //connect to database
+        // var url = "mongodb://localhost:27017/libraryapp";
+        // mongodb.connect(url,function(err,db){
 
-            var collection = db.collection('books');
-            collection.findOne({_id:id},function(err,result){
+        //     var collection = db.collection('books');
+        //     collection.findOne({_id:id},function(err,result){
+
+        //     res.render('bookView',{
+        //         book:result,
+        //         nav : nav
+
+
+        //      });
+
+        //      db.close();
+
+        //     });
+
+        // });
+
+
+
+        //Using mongoose to get book by ID
+        mongoose.connect('mongodb://localhost:27017/libraryapp');
+
+        BookData.findById(id,function(err,result){
 
             res.render('bookView',{
                 book:result,
-                nav : nav
-
-
-             });
-
-             db.close();
-
+                nav:nav
             });
 
+
         });
+
 
 
     }
